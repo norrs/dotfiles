db2d56c7738ed144d673d6e8e03678e4e65a37cb
Roy Sindre Norangshol <norangshol@zedge.net>
2019-06-13 07:21:07 +0200

    Add clickhousev2 to my helper functions

--- dot.lib/shellenv/clickhouse.sh
+++ dot.lib/shellenv/clickhouse.sh
@@ -22,3 +22,28 @@ run_on_clickhouse_server() {
  shift
  kubectl exec -ti "clickhouse-clickhouse-$shard" -- "$@"
 }
+
+
+run_on_clickhouse_v2_servers() {
+    for i in $(seq 0 "$NUM_SHARDS"); do
+        kubectl exec -ti "clickhousev2-clickhouse-$i" -- "$@"
+    done
+}
+
+clickhouse_v2_portforward() {
+ (set -x; kubectl port-forward clickhousev2-clickhouse-0 9000)
+}
+
+
+
+clickhouse_v2_portforward_prod() {
+ (set -x; kubectl --context env:analytics port-forward clickhousev2-clickhouse-0 9000)
+}
+
+run_on_clickhouse_v2_server() {
+ local shard="$1"
+ test "$shard" -ge 0 || { >&2 echo "ERR: Only have clickhouse shards from 0 to $NUM_SHARDS"; return -1; }
+ test "$shard" -le "$NUM_SHARDS" || { >&2 echo "ERR: Only have clickhouse shards from 0 to $NUM_SHARDS"; return -1; }
+ shift
+ kubectl exec -ti "clickhousev2-clickhouse-$shard" -- "$@"
+}
