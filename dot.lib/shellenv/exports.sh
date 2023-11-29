04a919a16c0d1e89f65335a297d2ac8a99e99a7f
Roy Sindre Norangshol <roy.sindre@norangshol.no>
2023-11-29 10:15:43 +0100

    use rtx for goroot

--- dot.lib/shellenv/exports.sh
+++ dot.lib/shellenv/exports.sh
@@ -1,6 +1,6 @@
 export KUBECD_ENVIRONMENTS="$HOME/.config/kcd/environments.yaml"
 export GEM_HOME="$HOME/.gems"
 export GOPATH="$HOME/go"
-export GOROOT="$HOME/.local/opt/go"
+#export GOROOT="$HOME/.local/opt/go"
 export GO111MODULE="on"
 export JAVA_HOME="/usr/lib/jvm/default-java" # sudo update-java-alternatives ...
