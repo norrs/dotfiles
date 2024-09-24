7e2b3de0d6aeffeb885cd2530a59261fff7a8773
Roy Sindre Norangshol <roy.sindre@norangshol.no>
2024-09-24 15:13:18 +0200

    Update jwtparse*

--- dot.lib/shellenv/webdev.sh
+++ dot.lib/shellenv/webdev.sh
@@ -1,5 +1,15 @@
 
 jwtparse() {
-  jq -R 'gsub("-";"+") | gsub("_";"/") | split(".") | .[1] | @base64d | fromjson' <<<"$@"
+  jq -R 'gsub("-";"+") | gsub("_";"/") | split(".") | .[1] | @base64d | fromjson'
+}
+
+# extract header and payload
+jwtparse_all() {
+  jq -R 'gsub("-";"+") | gsub("_";"/") | split(".") |  select(length > 0) | .[0],.[1] | @base64d | fromjson'
+}
+
+# extract signature
+jwtparse_signature() {
+  jq -R 'gsub("-";"+") | gsub("_";"/") | split(".") |  select(length > 0) | .[2]' -r
 }
 
